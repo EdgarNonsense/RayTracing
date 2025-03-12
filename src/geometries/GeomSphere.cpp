080eda2f02db7c01ac76a1dc1bb47fe918db23bc
EdgarNonsense <mc2ethan@gmail.com>
2025-03-11 20:17:16 -0700

    Step 1-3

--- src/geometries/GeomSphere.cpp
+++ src/geometries/GeomSphere.cpp
@@ -6,7 +6,7 @@
 #include "Intersection.h"
 #include "Ray.h"
 
-std::vector<Intersection> GeomSphere::intersect(Ray &ray) {
+std::vector<Intersection> GeomSphere::intersect(Ray& ray) {
     /**
      * NOTE: Ray is already transformed to the Model coordinate space.
      */
@@ -18,6 +18,45 @@ std::vector<Intersection> GeomSphere::intersect(Ray &ray) {
     /**
      * TODO: Implement the Ray intersection with the current geometry
      */
+     // SPHERE
+    vec3 center = this->center;
+    float radius = this->radius;
+
+    // RAY
+    vec3 origin = ray.p0;
+    vec3 direction = normalize(ray.dir);
+
+    vec3 p0_c = origin - center;
+    float dir_p0c = dot(direction, p0_c);
+    float dir_p0c_squared = dir_p0c * dir_p0c;
+    float c_term = dot(p0_c, p0_c) - (radius * radius);
+    float interior = dir_p0c_squared - c_term;
+
+    // If inside sqrt is negative then no intersection
+    if (interior < 0) {
+        return intersections;
+    }
+
+    float sqrt_discriminant = sqrt(interior);
+    float t1 = -dir_p0c - sqrt_discriminant;
+    float t2 = -dir_p0c + sqrt_discriminant;
+
+    // If interior is 0, add the tangent
+    if (interior == 0) {
+        if (t1 > 0) {
+            vec3 point = origin + t1 * direction;
+            vec3 normal = normalize(point - center);
+            intersections.push_back({ t1, point, normal, this, nullptr });
+        }
+        return intersections;
+    }
+
+    // If interior is positive then take the smallest positive t
+    float smallest_positive_t = (t1 > 0 && (t1 < t2 || t2 <= 0)) ? t1 : t2;
+
+    vec3 point = origin + smallest_positive_t * direction;
+    vec3 normal = normalize(point - center);
+    intersections.push_back({ smallest_positive_t, point, normal, this, nullptr });
 
     /**
      * Once you find the intersection, add it to the `intersections` vector.
@@ -37,9 +76,9 @@ std::vector<Intersection> GeomSphere::intersect(Ray &ray) {
      *   where t > 0.
      */
 
-    /**
-     * TODO: Update `intersections`
-     */
+     /**
+      * TODO: Update `intersections`
+      */
 
     return intersections;
 };
--- src/geometries/GeomTriangle.cpp
+++ src/geometries/GeomTriangle.cpp
@@ -9,7 +9,7 @@
 #include "Intersection.h"
 #include "Ray.h"
 
-GeomTriangle::GeomTriangle(std::vector<glm::vec3> &vertices, std::vector<glm::vec3> &normals) {
+GeomTriangle::GeomTriangle(std::vector<glm::vec3>& vertices, std::vector<glm::vec3>& normals) {
     this->vertices[0] = vertices[0];
     this->vertices[1] = vertices[1];
     this->vertices[2] = vertices[2];
@@ -19,40 +19,84 @@ GeomTriangle::GeomTriangle(std::vector<glm::vec3> &vertices, std::vector<glm::ve
     this->normals[2] = normals[2];
 }
 
-std::vector<Intersection> GeomTriangle::intersect(Ray &ray) {
+std::vector<Intersection> GeomTriangle::intersect(Ray& ray) {
     using namespace glm;
     /**
      * NOTE: Ray is already transformed to the Model coordinate space.
      */
 
-    // vector to store the intersections
+     // vector to store the intersections
     std::vector<Intersection> intersections;
+
+    vec3 p1 = vertices[0];
+    vec3 p2 = vertices[1];
+    vec3 p3 = vertices[2];
+
+    vec3 p0 = ray.p0;
+    vec3 d = ray.dir;
+
+    // coefficient matrix
+    mat4 A(
+        vec4(p1, 1.0f),
+        vec4(p2, 1.0f),
+        vec4(p3, 1.0f),
+        vec4(-d, 0.0f)
+    );
+
+    // Vector b
+    vec4 b(p0, 1.0f);
+
+    // Solving the lambda/t vector then getting their values
+    vec4 x = inverse(A) * b;
+
+    float lambda1 = x.x;
+    float lambda2 = x.y;
+    float lambda3 = x.z;
+    float t = x.w;
+
+    const float ep = 1e-5f;
+    float sum = lambda1 + lambda2 + lambda3;
+
+    // Check if intersection is valid (inside the triangle and in front of the camera)
+    if (t >= 0 && lambda1 >= 0 && lambda2 >= 0 && lambda3 >= 0 && abs(sum - 1.0f) <= ep) {
+        // intersection point
+        vec3 q = lambda1 * p1 + lambda2 * p2 + lambda3 * p3;
+
+        // normal
+        vec3 n1 = normals[0];
+        vec3 n2 = normals[1];
+        vec3 n3 = normals[2];
+
+        vec3 normal = normalize(lambda1 * n1 + lambda2 * n2 + lambda3 * n3);
+
+        intersections.push_back({ t, q, normal, this, nullptr });
+    }
 
     /**
      * TODO: Implement the Ray intersection with the current geometry
      */
 
-    /**
-     * Once you find the intersection, add it to the `intersections` vector.
-     *
-     * Example:
-     * Suppose the ray intersects the current geometry at a point `vec3 point`
-     * at a distance `float t`, and the unit normal vector at the intersection
-     * point is `vec3 normal`. You would then insert an intersection into the
-     * vector as follows:
-     *
-     * intersections.push_back({t, point, normal, this, nullptr});
-     *
-     * Note:
-     * - Here we pass the Model pointer as `nullptr` because it will be
-     *   populated by the Model::intersect() function call.
-     * - Only add intersections that are in front of the camera, i.e.,
-     *   where t > 0.
-     */
+     /**
+      * Once you find the intersection, add it to the `intersections` vector.
+      *
+      * Example:
+      * Suppose the ray intersects the current geometry at a point `vec3 point`
+      * at a distance `float t`, and the unit normal vector at the intersection
+      * point is `vec3 normal`. You would then insert an intersection into the
+      * vector as follows:
+      *
+      * intersections.push_back({t, point, normal, this, nullptr});
+      *
+      * Note:
+      * - Here we pass the Model pointer as `nullptr` because it will be
+      *   populated by the Model::intersect() function call.
+      * - Only add intersections that are in front of the camera, i.e.,
+      *   where t > 0.
+      */
 
-    /**
-     * TODO: Update `intersections`
-     */
+      /**
+       * TODO: Update `intersections`
+       */
 
     return intersections;
 }
